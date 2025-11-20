e9e5c257ac9e33f6f1bda6e90400c8404333ea3c
Keelner1 <155470884+Keelner1@users.noreply.github.com>
2025-11-20 23:49:02 +0100

    Change Twitch channel from 'keelner2' to 'Nimuena_'

--- save-twitch-chat.js
+++ save-twitch-chat.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 async function run() {
-  const channel = "keelner2"; // podaj swój kanał Twitch
+  const channel = "Nimuena_"; // podaj swój kanał Twitch
   const url = `https://recent-messages.robotty.de/api/v2/recent-messages/${channel}`;
   const response = await fetch(url);
   const data = await response.json();
@@ -27,4 +27,5 @@ async function run() {
 }
 
 run();
+
 
